e988eb3a53d6080a7227875250d6e28591497d87
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-11-05 10:39:33 +0200

    import math python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+import math
+
+number1 = 10
+number2 = 5
+print ("Addition of 10 and 5 is ", number1 + number2)
+print ("Subtraction of 10 and 5 is ", number1 - number2)
+print ("Multiplication of 10 and 5 is ", number1 * number2)

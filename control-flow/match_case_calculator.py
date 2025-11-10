00dad7eed923b5b4a3ee06ce8f52257074043bce
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-11-10 10:13:22 +0200

    matching cases

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+if operation == "+":
+    result = num1 + num2
+    print(f"The result is: {result}")   
+elif operation == "-":
+    result = num1 - num2
+    print(f"The result is: {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"The result is: {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result is: {result}")
+    else:
+        print("Error: Division by zero is not allowed.")      

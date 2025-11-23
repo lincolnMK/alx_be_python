2e2087f7ab28e977c5463d67ec734f4b5c055481
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-11-23 22:53:43 +0200

    timing

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,5 +1,19 @@
 from datetime import date
-d= d
-print(d.year)
-print(d.month)
-print(d.day)
+from datetime import timedelta
+from datetime import datetime
+
+
+def  display_current_datetime():
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return current_date    
+
+def calculate_future_date():
+    days_ahead = int(input ("Enter the number of days to add to the current date:"))
+    current_date = date.today()
+    future_date = current_date + timedelta(days=days_ahead)
+    return future_date
+    
+current_date =  display_current_datetime()
+print("Current Date and Time:", current_date )
+future_date = calculate_future_date()
+print ("Future date:", future_date)

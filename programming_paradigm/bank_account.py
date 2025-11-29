1b5bc44ce7e2d6b079386d24f52907a3fe7924e5
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-11-29 22:35:13 +0200

    clases

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_balance ):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.account_balance:
+                self.account_balance -= amount
+                return True
+                
+            else:
+                return False
+        else:
+            print("Withdrawal amount must be positive.")
+
+    def display_balance(self):
+        print(self.account_balance)
